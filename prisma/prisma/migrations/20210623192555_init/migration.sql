45bbffbe5a501eae24177bd8bd4b214795b85c3d
joey desjardins <jwdesjardin@gmail.com>
2021-06-26 12:36:02 -0700

    attempt to add a new column to database met with error of having two columns of the same name: ending up reseting db with previous commit

--- prisma/prisma/migrations/20210623192555_init/migration.sql
+++ prisma/prisma/migrations/20210623192555_init/migration.sql
@@ -35,7 +35,6 @@ CREATE TABLE "Category" (
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "name" VARCHAR(255) NOT NULL,
     "image" VARCHAR(255) NOT NULL,
-
     PRIMARY KEY ("id")
 );
 
--- prisma/prisma/seed.ts
+++ prisma/prisma/seed.ts
@@ -4,128 +4,126 @@ import axios from 'axios'
 const prisma = new PrismaClient()
 import myData from '../data.json'
 
-
 const categorySwitch = (categorySTR: string) => {
-  switch(categorySTR){
-    case 'earphones':
-      return 1
-    case 'headphones':
-      return 2
-    case 'speakers':
-      return 3
-    default: 
-    return 0
-  }
+	switch (categorySTR) {
+		case 'earphones':
+			return 1
+		case 'headphones':
+			return 2
+		case 'speakers':
+			return 3
+		default:
+			return 0
+	}
 }
 
-const simpleFunc = async() => {
-  console.log(myData)
- 
+const simpleFunc = async () => {
+	console.log(myData)
 }
-
 
 const seedingFunction = async () => {
-  
-  
-  
-  myData.forEach(async (product: any) => {
-    const created = await prisma.product.upsert({
-      where: { slug: product.slug },
-      update: {},
-      create: {
-        name: product.name,
-        cartName: product.name.split(' ')[0],
-        description: product.description,
-        features: product.features,
-        cartImage: `./assets/cart/image-${product.slug}.jpg`,
-        galleryImageDesktop1: product.gallery.first.desktop,
-        galleryImageDesktop2: product.gallery.second.desktop,
-        galleryImageDesktop3: product.gallery.third.desktop,
-        galleryImageMobile1: product.gallery.first.mobile,
-        galleryImageMobile2: product.gallery.second.mobile,
-        galleryImageMobile3: product.gallery.third.mobile,
-        galleryImageTablet1: product.gallery.first.tablet,
-        galleryImageTablet2: product.gallery.second.tablet,
-        galleryImageTablet3: product.gallery.third.tablet,
-        mainImageDesktop: product.image.desktop,
-        mainImageMobile: product.image.mobile,
-        mainImageTablet: product.image.tablet,
-        price: product.price,
-        slug: product.slug,
-        categoryId: categorySwitch(product.category) ,
-        recommendations: {
-          create: product.others.map((other: any) => ({
-            name: other.name,
-            imageMobile: other.image.mobile,
-            imageTablet: other.image.tablet,
-            imageDesktop: other.image.desktop,
-            slug: other.slug,
-          })),
-        },
-        includedItems: {
-          create: product.includes.map((included: any) => ({
-            name: included.item,
-            quantity: included.quantity
-          }))
-        },
-      }
-    })
-  })
-
+	myData.forEach(async (product: any) => {
+		const created = await prisma.product.upsert({
+			where: { slug: product.slug },
+			update: {},
+			create: {
+				name: product.name,
+				cartName: product.name.split(' ')[0],
+				description: product.description,
+				features: product.features,
+				cartImage: `./assets/cart/image-${product.slug}.jpg`,
+				galleryImageDesktop1: product.gallery.first.desktop,
+				galleryImageDesktop2: product.gallery.second.desktop,
+				galleryImageDesktop3: product.gallery.third.desktop,
+				galleryImageMobile1: product.gallery.first.mobile,
+				galleryImageMobile2: product.gallery.second.mobile,
+				galleryImageMobile3: product.gallery.third.mobile,
+				galleryImageTablet1: product.gallery.first.tablet,
+				galleryImageTablet2: product.gallery.second.tablet,
+				galleryImageTablet3: product.gallery.third.tablet,
+				mainImageDesktop: product.image.desktop,
+				mainImageMobile: product.image.mobile,
+				mainImageTablet: product.image.tablet,
+				price: product.price,
+				slug: product.slug,
+				categoryId: categorySwitch(product.category),
+				recommendations: {
+					create: product.others.map((other: any) => ({
+						name: other.name,
+						imageMobile: other.image.mobile,
+						imageTablet: other.image.tablet,
+						imageDesktop: other.image.desktop,
+						slug: other.slug,
+					})),
+				},
+				includedItems: {
+					create: product.includes.map((included: any) => ({
+						name: included.item,
+						quantity: included.quantity,
+					})),
+				},
+			},
+		})
+	})
 }
 
-
-
-
 async function main() {
+	// RESET DATA DATABASE
+	await prisma.product.deleteMany()
+	await prisma.category.deleteMany()
 
-  
+	await prisma.includedItem.deleteMany()
+	await prisma.recommendation.deleteMany()
 
-  const category1 = await prisma.category.upsert({
-    where: { id: 1 },
-    update: {},
-    create: {
-      name: 'Earphones',
-      image: './assets/shared/desktop/image-earphones.png'
-    }
-  })
-  const category2 = await prisma.category.upsert({
-    where: { id: 2 },
-    update: {},
-    create: {
-      name: 'Headphones',
-      image: './assets/shared/desktop/image-headphones.png'
-    }
-  })
-  const category3 = await prisma.category.upsert({
-    where: { id: 3 },
-    update: {},
-    create: {
-      name: 'Speakers',
-      image: './assets/shared/desktop/image-speakers.png'
-    }
-  })
+	const category1 = await prisma.category.upsert({
+		where: { id: 1 },
+		update: {},
+		create: {
+			name: 'Earphones',
+			slug: 'earphones',
+			image: './assets/shared/desktop/image-earphones.png',
+		},
+	})
 
+	const category2 = await prisma.category.upsert({
+		where: { id: 2 },
+		update: {},
+		create: {
+			name: 'Headphones',
+			slug: 'headphones',
+			image: './assets/shared/desktop/image-headphones.png',
+		},
+	})
 
-  await seedingFunction()
+	const category3 = await prisma.category.upsert({
+		where: { id: 3 },
+		update: {},
+		create: {
+			name: 'Speakers',
+			slug: 'speakers',
+			image: './assets/shared/desktop/image-speakers.png',
+		},
+	})
 
-  const products = await prisma.product.findMany({
-    include: {
-      recommendations: true,
-      includedItems: true
-    }
-  })
+	console.log(category1, category2, category3)
 
-  console.log(products)
-  
-  
+	await seedingFunction()
+
+	const products = await prisma.product.findMany({
+		include: {
+			recommendations: true,
+			includedItems: true,
+		},
+	})
+
+	console.log(products)
 }
 
 main()
-  .catch(e => {
-    console.error(e)
-    process.exit(1)
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+	.catch((e) => {
+		console.error(e)
+		process.exit(1)
+	})
+	.finally(async () => {
+		await prisma.$disconnect()
+	})
