1086de7cf6d7d09b1d09f6465723bb986154cd0a
joey desjardins <jwdesjardin@gmail.com>
2021-07-10 10:58:11 -0700

    combine users server into orders server

---
+++ audiophile-orders/controllers/users.ts
@@ -0,0 +1,53 @@
+import asyncHandler from 'express-async-handler'
+import User, { UserType } from '../models/user'
+
+export const registerUser = asyncHandler(async (req, res) => {
+	const { email, password, name } = req.body
+
+	// if their is a user with this email respond with error
+	const userExists: UserType = await User.findOne({ email })
+
+	if (userExists) {
+		res.status(400)
+		throw new Error('User already exists')
+	}
+
+	const user: UserType = await User.create({ name, email, password })
+
+	if (user) {
+		// 201 - user was created  - respond with user data plus generated token
+		console.log(user)
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		})
+	} else {
+		// error - user was unable to be created
+		res.status(400)
+		throw new Error('Invalid user data')
+	}
+})
+
+export const authUser = asyncHandler(async (req, res) => {
+	const { email, password } = req.body
+
+	// find a user that matches email
+	const user: UserType = await User.findOne({ email })
+
+	// if there is a user and the password matches
+	if (user && (await user.matchPassword(password))) {
+		//respond with the logged in users data plus a generated token
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		})
+	} else {
+		//if there is not valid email, password
+		res.status(401)
+		throw new Error('Invalid email or password')
+	}
+})
--- audiophile-orders/server.ts
+++ audiophile-orders/server.ts
@@ -3,6 +3,9 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import { postAnOrder, getAllOrders, getUserOrders } from './controllers/order'
 import { getAllCustomers } from './controllers/customer'
+import { errorHandler, notFound } from './lib/error'
+import connectDB from './lib/db'
+import { authUser, registerUser } from './controllers/users'
 
 dotenv.config()
 
@@ -15,7 +18,7 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
-	res.send('Welcome to the Audiophile Orders API')
+	res.send('Welcome to the Audiophile Orders/Users API')
 })
 
 // order Routes
@@ -27,7 +30,18 @@ app.get('/customer', getAllCustomers)
 
 app.get('/order/:id', getUserOrders)
 
+// connect auth db Mongo
+connectDB()
+
+// authentication routes
+app.post('/users/login', authUser)
+app.post('/users/', registerUser)
+
 const PORT = process.env.PORT || 4000
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
 	console.log(`Server is listening on port ${PORT}`)
